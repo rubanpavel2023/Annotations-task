a59ce1132917ea4e5ac15683e5ff482325d25423
Pavel <146098297+rubanpavel2023@users.noreply.github.com>
2024-01-15 11:40:09 +0200

    Initial commit

---
+++ src/app/ProductHandler.java
@@ -0,0 +1,34 @@
+package app;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Method;
+
+public class ProductHandler {
+
+    public static void main(String[] args) throws Exception {
+        new ProductInfo();
+        Class<ProductInfo> infoClass = ProductInfo.class;
+        getAnnot(infoClass);
+        Method method = infoClass.getMethod("getData");
+        getAnnot(method);
+    }
+
+    static void getAnnot(AnnotatedElement element) {
+
+        try {
+
+            Annotation[] annotations = element.getAnnotations();
+
+            for (Annotation annotation : annotations) {
+                if (annotation instanceof Product product) {
+                    System.out.printf("Product: %s, quota: %d%n",
+                            product.name(), product.quota());
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+        }
+    }
+}
